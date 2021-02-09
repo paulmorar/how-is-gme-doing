ec08ab0bb92df93de91f93b698dd6d9b94845c09
Paul Morar <paulcornelmorar@gmail.com>
2021-02-09 12:54:03 +0100

    Create rocket

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import rocket from './rocket.svg';
 import './App.css';
 
-function App() {
+const BASE_URL = "";
+const BASE_ROT_VAL = 45;
+const TICKER = "GME";
+
+const getRandNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const App = () => {
+  const [angleValue, setAngleValue] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     const result = await axios(`${BASE_URL}&symbols=${TICKER}`)
+  //     const percentageChange = result.quoteResponse.result.regularMarketChangePercent || 0;
+  //     console.log({percentageChange, result})
+  //     setAngleValue(getRandNumber(-90, 90))
+  //   }, 3000);
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img style={{ transform: `rotate(${BASE_ROT_VAL + angleValue}deg)`}} src={rocket} className="App-pointer" alt="rocket" />
+        <p>GME live moon ride!</p>
       </header>
     </div>
   );
